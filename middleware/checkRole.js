6905a506c0dac751c6d80626ffd3cfa9c8369f62
Daniil-1178 <daniilzubkov278@gmail.com>
2025-12-16 22:59:55 +0200

    Update checkRole.js

--- middleware/checkRole.js
+++ middleware/checkRole.js
@@ -5,7 +5,7 @@ module.exports = (requiredRole) => {
         }
 
         if (req.session.userRole !== requiredRole) {
-            console.log(`⚠️ Відмовлено в доступі: Користувач має роль ${req.session.userRole}, а потрібна ${requiredRole}`);
+            console.log(`Відмовлено в доступі: Користувач має роль ${req.session.userRole}, а потрібна ${requiredRole}`);
             return res.status(403).send('Доступ заборонено: у вас немає прав адміністратора.');
         }
 
